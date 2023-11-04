4cef31de586e5bb2367ab2f34830e56ce023f12d
swathi <uttaralaswathi1998@gmail.com>
2023-11-04 09:20:40 +0000

    travelguide

--- src/components/TravelGuide/index.js
+++ src/components/TravelGuide/index.js
@@ -1,82 +1,15 @@
-import {Component} from 'react'
-import Loader from 'react-loader-spinner'
-import TravelPlacesList from '../TravelPlacesList'
 import './index.css'
 
-const apiStatusConstants = {
-  initial: 'INITIAL',
-  success: 'SUCCESS',
-  failure: 'Failure',
-  inProgress: 'IN_PROGRESS',
-}
-
-class TravelGuide extends Component {
-  state = {
-    placesList: [],
-    apiStatus: apiStatusConstants.initial,
-  }
-
-  componentDidMount() {
-    this.getTravelPlaces()
-  }
-
-  getTravelPlaces = async () => {
-    this.setState({apiStatus: apiStatusConstants.inProgress})
-    const apiUrl = 'https://apis.ccbp.in/tg/packages'
-    const response = await fetch(apiUrl)
-    if (response.ok === true) {
-      const fetchedData = await response.json()
-      const updatedList = fetchedData.packages.map(each => ({
-        id: each.id,
-        name: each.name,
-        imageUrl: each.image_url,
-        description: each.description,
-      }))
-      console.log(updatedList)
-      this.setState({
-        placesList: updatedList,
-        apiStatus: apiStatusConstants.success,
-      })
-    }
-  }
-
-  renderLoader = () => (
-    <div data-testid="loader" className="position">
-      <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
-    </div>
+const TravelGuide = props => {
+  const {details} = props
+  const {name, imageUrl, description} = details
+  return (
+    <li className="li-ei">
+      <img src={imageUrl} className="ima" alt={name} />
+      <h1 className="name">{name}</h1>
+      <p className="desc">{description}</p>
+    </li>
   )
-
-  renderPlacesView = () => {
-    const {placesList} = this.state
-    return (
-      <ul className="list-container">
-        {placesList.map(each => (
-          <TravelPlacesList key={each.id} eachPlace={each} />
-        ))}
-      </ul>
-    )
-  }
-
-  renderViewBasedOnApiStatus = () => {
-    const {apiStatus} = this.state
-    switch (apiStatus) {
-      case apiStatusConstants.success:
-        return this.renderPlacesView()
-      case apiStatusConstants.inProgress:
-        return this.renderLoader()
-      default:
-        return null
-    }
-  }
-
-  render() {
-    return (
-      <div className="travel-guide-container">
-        <h1 className="main-heading">Travel Guide</h1>
-        <hr className="separator" />
-        {this.renderViewBasedApiStatus()}
-      </div>
-    )
-  }
 }
+
 export default TravelGuide
